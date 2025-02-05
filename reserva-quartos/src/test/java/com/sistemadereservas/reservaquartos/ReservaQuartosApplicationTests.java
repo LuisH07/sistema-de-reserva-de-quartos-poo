c342d481886d78f13ab7279cb1a78bb27e195ebd
Luis <luishenriqueds01@gmail.com>
2025-02-04 22:48:27 -0300

    estrutura inicial do projeto

---
+++ reserva-quartos/src/test/java/com/sistemadereservas/reservaquartos/ReservaQuartosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sistemadereservas.reservaquartos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservaQuartosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
